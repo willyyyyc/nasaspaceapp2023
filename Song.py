0cb6b97c0e0a67bf346f6537642999d0a4ec718c
naman108 <natha.paquette@gmail.com>
2023-10-07 18:20:48 -0300

    implemented song creation

--- Song.py
+++ Song.py
@@ -1,12 +1,7 @@
 class Song:
-    def __init__(self, flags):
-        self.flags = flags
-
-    def find_flag(self, flag):
-        if flag in self.flags:
-            return self.flags[flag]
-    
-    
-    
-    
-            
+    def __init__(self, sounds):
+        self.sounds = sounds
+        self.song = sounds[0].sound
+        
+        for sound in sounds[1:]:
+            self.song + sound.sound
